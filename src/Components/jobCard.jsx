0c583a61596cc93f251eefcc98797334da46c42f
Nikhil Gupta <nikhil.gupta@Savvytechies.com>
2024-05-04 14:45:33 +0530

    created czrd

--- src/Components/jobCard.jsx
+++ src/Components/jobCard.jsx
@@ -5,18 +5,21 @@ const JobCard = ({ job }) => {
   return (
     <div className="job-card">
       <div className="company-info">
-        <img src={job.logoUrl} alt={job.companyName} className="company-logo" />
+        <img src={job?.logoUrl} alt={job?.companyName} className="company-logo" />
         <div className="company-details">
-          <p className="company-name">{job.companyName}</p>
-          <p>{job.jobRole}</p>
+          <p className="company-name">{job?.companyName}</p>
+          <p>{job?.jobRole}</p>
         </div>
       </div>
       <p className="location-exp">
-        {job.location} | Exp: {job.minExp}-{job.maxExp} years
+        {job?.location} | Exp: {job?.minExp}-{job?.maxExp} years
       </p>
-      <p className="salary">Estimated Salary: {job.minJdSalary || '-'} LPA - {job.maxJdSalary || '-'} LPA</p>
+      <p className="salary">
+        Estimated Salary: {job?.minJdSalary || '-'} LPA - {job?.maxJdSalary || '-'} LPA
+      </p>
+      <p style={{fontWeight:"bold",marginBottom:"0"}}>About Company:</p>
       <p className="about-us">About Us:</p>
-      <p>{job.jobDetailsFromCompany}</p>
+      <p style={{marginTop:"0"}}>{job?.jobDetailsFromCompany}</p>
       <div className="skills">
         <p>Skills:</p>
         <div className="skill-list">
